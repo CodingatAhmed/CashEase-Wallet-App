cc0ca1c628b21b87e78f12fccdb9c2443e00b9c2
Ahmed Abdul Rehman <ahmedabdulrehman2005@gmail.com>
2025-10-01 12:38:05 +0500

    newChanges

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react@0.487.0";
-import { DayPicker } from "react-day-picker@8.10.1";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { DayPicker } from "react-day-picker";
 
 import { cn } from "./utils";
 import { buttonVariants } from "./button";
@@ -60,12 +60,12 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ ...props }: any) =>
+          (props.direction) === "left" ? (
+            <ChevronLeft className={cn("h-4 w-4", props.className)} />
+          ) : (
+            <ChevronRight className={cn("h-4 w-4", props.className)} />
+          ),
       }}
       {...props}
     />
